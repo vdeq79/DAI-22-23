c4151651570eedf44b95692a6152f7b9fd116926
vdeq79 <xszh79@outlook.com>
2022-09-16 17:03:12 +0200

    Subida de P0

--- P0/ejercicio4.py
+++ P0/ejercicio4.py
@@ -1,6 +1,14 @@
 import re
 
 def check1(str):
-    return re.search(r'(?!\s)\s[A-Z]\Z',str)
+    return re.search(r'^[a-zA-Z0-9_]+\s[A-Z]\Z',str)
+
+def check2(str):
+    return re.search(r'^[^\s@]+@[^\s@]+\.[^\s@]{2,}',str)
+
+def check3(str):
+    return re.search(r'[0-9]{4}[\s-][0-9]{4}[\s-][0-9]{4}[\s-][0-9]{4}',str)
 
 print(check1("Apellido N"))
+print(check2("a@ dlo.com"))
+print(check3("1234 5678 9012 3456"))
