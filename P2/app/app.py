70b70c7f668b43488230cd2d3c907bf51bf5168a
vdeq79 <xszh79@outlook.com>
2022-10-06 13:05:58 +0200

    P2.2 v0.3

--- P2/app/app.py
+++ P2/app/app.py
@@ -1,10 +1,13 @@
+from urllib import response
 from flask import Flask, send_from_directory, render_template, Response, request, jsonify
+from flask_restful import Resource, Api, reqparse
 from bson import ObjectId
 from bson.json_util import dumps
 from pymongo import MongoClient
 from math import isqrt
 import random
 import re
+import json
 # Conectar al servicio (docker) "mongo" en su puerto estandar
 client = MongoClient("mongo", 27017) 
 
@@ -13,8 +16,7 @@ db = client.cockteles
 
 app = Flask(__name__)
 
-import api
-
+api = Api(app)
 #<----------------------------------------------------------------------------------------------------------------------------------->
 @app.route('/')
 def hello_world():
@@ -149,34 +151,54 @@ def mongo():
   return Response(response, mimetype='application/json')
 
 #<----------------------------------------------------------------------------------------------------------------------------------->
-@app.route('/recetas_de/cuba_libre')
-def cuba_libre():
+@app.route('/recetas_de/<name>')
+def recetas_de(name):
 
-  recetas = db.recipes.find({"name":"Cuba Libre"})
+  recetas = db.recipes.find({"name":name})
   response = extraerDatosMongo(recetas)
 
   # Devolver en JSON al cliente
   return Response(response, mimetype='application/json')
 
 #<----------------------------------------------------------------------------------------------------------------------------------->
-@app.route('/recetas_con/vodka')
-def con_vodka():
+@app.route('/recetas_con/<name>')
+def recetas_con(name):
 
-  recetas = db.recipes.find({"ingredients.name":"Vodka"})
+  recetas = db.recipes.find({"ingredients.name":name})
   response = extraerDatosMongo(recetas)
 
   # Devolver en JSON al cliente
   return Response(response, mimetype='application/json')
 
 #<----------------------------------------------------------------------------------------------------------------------------------->
-@app.route('/recetas_compuestas_de/2/ingredientes')
-def dos_ingredientes():
+@app.route('/recetas_compuestas_de/<int:n>/ingredientes')
+def compuestas_de_num(n):
 
-  recetas = db.recipes.find({"ingredients":{"$size":2}})
+  recetas = db.recipes.find({"ingredients":{"$size":n}})
   response = extraerDatosMongo(recetas)
 
   # Devolver en JSON al cliente
   return Response(response, mimetype='application/json')
+
+
+#<----------------------------------------------------------------------------------------------------------------------------------->
+@app.route('/recetas_compuestas_de/<path:varargs>')
+def compuestas_de(varargs=None):
+  varargs = varargs.split("/")
+
+  #Eliminar las slashes vacías
+  try:
+    while True:
+      varargs.remove("")
+  except ValueError:
+    pass
+
+  recetas = db.recipes.find({"ingredients.name":{"$all":varargs}})
+  response = extraerDatosMongo(recetas)
+
+  # Devolver en JSON al cliente
+  return Response(response, mimetype='application/json')
+  #return varargs
 
 #<----------------------------------------------------------------------------------------------------------------------------------->
 # para devolver una lista (GET), o añadir (POST)
@@ -224,3 +246,78 @@ def api_2(id):
           return id
       else:
         return jsonify({'error':'Not found'}), 404
+
+#<----------------------------------------------------------------------------------------------------------------------------------->
+recipe_parser = reqparse.RequestParser()
+recipe_parser.add_argument("name", type=str, help="El nombre es requerido" ,required=True)
+recipe_parser.add_argument("slug", type=str)
+recipe_parser.add_argument("ingredients", action='append', help="La lista de ingredientes es requerida" ,required=True)
+recipe_parser.add_argument("instructions", type=str, action='append' ,help="Las instrucciones son requeridas" ,required=True)
+
+
+put_parser = reqparse.RequestParser()
+put_parser.add_argument("$set", type=json ,help="El nombre es requerido" ,required=True)
+
+#<----------------------------------------------------------------------------------------------------------------------------------->
+# para devolver una lista (GET), o añadir (POST)
+class RecipeList(Resource):
+    def get(self):
+        lista = []
+        buscados = db.recipes.find().sort('name')
+        for recipe in buscados:
+            recipe['_id'] = str(recipe['_id']) # paso a string
+            lista.append(recipe)
+        return jsonify(lista)
+
+    def post(self):
+        args = recipe_parser.parse_args()
+        result = db.recipes.insert_one(args)
+        return str(result.inserted_id), 201
+
+
+#<----------------------------------------------------------------------------------------------------------------------------------->
+class Recipe(Resource):
+    def get(self, id):
+        buscado = db.recipes.find_one({'_id':ObjectId(id)})
+        if buscado:
+          buscado['_id'] = str(buscado['_id'])
+          response = jsonify(buscado)
+        else:
+          response = jsonify({'error':'Not found'})
+          response.status = 404
+        
+        return response
+
+    def put(self, id):
+        args = request.get_json()
+        result = db.recipes.update_one({'_id':ObjectId(id)}, args)
+
+        if(result.matched_count==1):
+          modificado = db.recipes.find_one({'_id':ObjectId(id)})
+          modificado['_id'] = str(modificado['_id'])
+          response = jsonify(modificado)
+          response.status = 201
+
+        else:
+          response = jsonify({'error':'Not found'})
+          response.status = 404
+        
+        return response
+
+    def delete(self, id):
+        result = db.recipes.delete_one({'_id':ObjectId(id)})
+
+        if(result.deleted_count==1):
+          return id, 202
+        else:
+          response = jsonify({'error':'Not found'})
+          response.status = 404
+          return response
+
+
+api.add_resource(RecipeList,'/RecipeList')
+api.add_resource(Recipe,'/Recipe/<id>')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
