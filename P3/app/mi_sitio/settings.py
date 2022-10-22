da6ca1658a829c0c2e4e435fecca3c5b85fbb1fd
vdeq79 <xszh79@outlook.com>
2022-10-22 18:07:32 +0200

    P3 v0.3

--- P3/app/mi_sitio/settings.py
+++ P3/app/mi_sitio/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mi_sitio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mi_sitio.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3') ,
     }
 }
 
@@ -116,8 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- P3/app/recetas/views.py
+++ P3/app/recetas/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import  HttpResponse
+from multiprocessing import context
+from .models import Receta
+from django.shortcuts import  HttpResponse, render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World!')
+    return render(request,'plantilla.html')
+
+def searchView(request):
+    query_dict = request.GET
+    query = query_dict.get('query')
+    
+    result = None
+
+    if query!= '':
+        result = Receta.objects.get(nombre=query)
+
+
+    context = {"result":result}
+    return render(request,'search.html', context=context)
