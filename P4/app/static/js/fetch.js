52ab6728bae6852327f2f5f8e36c8ebf44b868e0
vdeq79 <xszh79@outlook.com>
2022-12-06 23:01:01 +0100

    P4 0.2 implementa las operaciones CRUD

--- P4/app/static/js/fetch.js
+++ P4/app/static/js/fetch.js
@@ -9,25 +9,122 @@ fetch('/api/recipes')           // GET por defecto,
         i++
         recetas.push(fila)      // se guardan para después sacar cada una  
         // ES6 templates
-        html_str += `<tr>
+        html_str += ` <tr>
                        <td>${i}</td>
                        <td>
-                          <button onclick="detalle('${fila._id}')" 
+                          <button onclick="detalle('${i}')" 
                                 type="button" class="btn btn-outline btn-sm"
                                 data-bs-toggle="modal" data-bs-target="#detailModal">
                           ${fila.name}
-                       </button>
-                </td>
-                <td>
-                <button type="button" class="btn btn-warning btn-sm">Edit</button>
-                <button type="button" class="btn btn-danger btn-sm">Delete</button>
-                </td>
-                </tr>`         // ES6 templates
+                          </button>
+                        </td>
+                        <td>
+                          <button onclick="showEditModal('${i}')"
+                                  type="button" class="btn btn-warning btn-sm"
+                                  data-bs-toggle="modal" data-bs-target="#formModal">Edit
+                          </button>
+                          <button onclick="showDeleteModal('${i}')"
+                                  type="button" class="btn btn-danger btn-sm"
+                                  data-bs-toggle="modal" data-bs-target="#deleteModal">Delete
+                          </button>
+                        </td>
+                      </tr>`         // ES6 templates
     });
     document.getElementById('tbody').innerHTML=html_str  // se pone el html en su sitio
 })
 
+
 function detalle(i) {  // saca un modal con la información de cada coctel
   // saca un modal con receta[i]
-  console.log(i)
+  const receta=recetas[i-1];
+  document.getElementById('detailModalLabel').textContent = receta.name;
+
+  let html_str = ''
+  receta.ingredients.forEach((ingredient)=>{
+    html_str+= `<li>${ingredient.name}</li>`
+  })
+  document.getElementById('ingredientsList').innerHTML = html_str;
+
+  html_str = ''
+  receta.instructions.forEach((instruction)=>{
+    html_str+= `<dd>- ${instruction}</dd>`
+  })
+  document.getElementById('instructionsList').innerHTML = html_str;
+};
+
+function showEditModal(i){
+  const receta = recetas[i-1];
+  document.getElementById('formModalLabel').textContent = 'Edit Recipe';
+  document.getElementById('formName').value = receta.name;
+
+  let html_str = ''
+  receta.ingredients.forEach((ingredient, pos, arr)=>{
+    html_str+= ingredient.name;
+    if (pos!=arr.length-1)
+      html_str+='\n'
+  })
+  document.getElementById('formIngredients').value = html_str;
+
+  html_str = ''
+  receta.instructions.forEach((instruction, pos, arr)=>{
+    html_str+= instruction;
+    if (pos!=arr.length-1)
+      html_str+='\n'
+  })
+  document.getElementById('formInstructions').value = html_str;
+  document.getElementById('btSaveForm').value = i;
 }
+
+
+function showDeleteModal(i){
+  const receta = recetas[i-1];
+  document.getElementById('deleteMessage').textContent = 'Do you want to delete '+receta.name+' ?';
+  document.getElementById('btDelete').value=i;
+}
+
+function eliminar(i){
+  const receta = recetas[i-1];
+  fetch('/api/recipes/'+receta._id,{
+    method: 'DELETE'
+  })
+  .then(()=>window.location.reload())
+  .catch((err)=>alert(err));
+}
+
+document.getElementById('btDelete').addEventListener("click",event=>{
+  eliminar(event.target.value);
+});
+
+document.getElementById('btAdd').addEventListener("click",function(){
+  document.getElementById('recipeForm').reset();
+  document.getElementById('formModalLabel').textContent = 'Add Recipe';
+  document.getElementById('btSaveForm').value = 'add';
+})
+
+document.getElementById('btSaveForm').addEventListener("click",event=>{
+  let method = event.target.value=='add' ? 'POST': 'PUT';
+  let url = '/api/recipes';
+
+  let body = {
+    'name': document.getElementById('formName').value,
+    'ingredients': document.getElementById('formIngredients').value.split('\n').map(ingredient=>{return {'name':ingredient};}),
+    'instructions': document.getElementById('formInstructions').value.split('\n')
+  };
+
+  if (method=='PUT'){
+    url+='/'+recetas[event.target.value-1]._id;
+    body={'$set':body};
+  }
+
+  console.log(body);
+
+  fetch(url,{
+    method: method,
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify(body)
+  })
+  .then(()=>window.location.reload())
+  .catch((err)=>alert(err));
+})
